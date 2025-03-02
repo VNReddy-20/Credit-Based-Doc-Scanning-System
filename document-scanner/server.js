2467f4e47efe1d89109e35e87fa612bca16b96ed
Narendra Reddy .V <143719480+VNReddy-20@users.noreply.github.com>
2025-03-02 17:15:00 +0530

    Update server.js

--- document-scanner/server.js
+++ document-scanner/server.js
@@ -124,6 +124,7 @@ app.get('/admin/analytics', authenticateUser, (req, res) => {
     res.json(analytics);
 });
 
+
 // Start Server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
